cde7cd42fe86e558603d81f731ce6163b28a9b85
AngiCasella <acasella@danielcastelao.org>
2023-12-05 13:46:49 +0100

    Modificación de VM

--- app/src/main/java/com/dam/examencorutinas/JuegoViewModel.kt
+++ app/src/main/java/com/dam/examencorutinas/JuegoViewModel.kt
@@ -1,6 +1,9 @@
 package com.dam.examencorutinas
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel del juego cuenta atrás y frases.
@@ -18,6 +21,47 @@ class JuegoViewModel: ViewModel() {
         Datos.aux()
     }
 
+    /**
+     * Comprueba si la frase es verdadera o falsa.
+     */
+    fun comprobarFrase(respuesta: Boolean) {
+        if (respuesta == Datos.fraseActual.value.verdadero) {
+            Datos.puntuacion.value += 20
+        } else {
+            // fallo
+            Datos.puntuacion.value -= 10
+            if (Datos.puntuacion.value < 0) {
+                Datos.puntuacion.value = 0
+            }
+        }
+        Datos.aux()
+    }
 
+    /**
+     * Cuenta atrás.
+     */
+    fun cuentaAtras() {
+        Datos.state = State.JUGANDO
+        viewModelScope.launch {
+            while (Datos.cuentaAtras.value > 0) {
+                delay(1000)
+                Datos.cuentaAtras.value--
+            }
+            Datos.state = State.ESPERANDO
+        }
+    }
 
+    /**
+     * Comprueba si la cuenta atrás ha terminado.
+     */
+    fun comprobarCuentaAtras(): Boolean {
+        return Datos.cuentaAtras.value == 0
+    }
+
+    /**
+     * Asigna una frase aleatoria.
+     */
+    fun asignarFraseAleatoria() {
+        Datos.fraseActual.value = Datos.frases.random()
+    }
 }
